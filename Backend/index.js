dcb6cf6a0d508f5dde51ff631e543f5c8f674631
Olivier <omalahel@gmail.com>
2025-04-08 10:24:49 +0200

    create deploy file

--- Backend/index.js
+++ Backend/index.js
@@ -10,15 +10,16 @@ const fs = require("fs");
 const app = express();
 const PORT = 4000;
 
-var usersRouter = require('./routes/user');
 var announcesRouter = require('./routes/announce')
 var contactRouter = require('./routes/contact')
+var loginRouter = require('./routes/login')
 
 app.use(cors());
 app.use(express.json());
-app.use('/users', usersRouter);
+
 app.use('/announces', announcesRouter);
 app.use("/contact", contactRouter);
+app.use('/connect',loginRouter)
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {
   setHeaders: (res, path) => {
---
+++ Backend/routes/login.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+const multer = require("multer");
+const bcrypt = require("bcrypt");
+
+
+
+router.post("/signin", (req, res) => {
+  if (req.body.password === process.env.ADMIN_PASSWORD) {
+    return res.status(200).json({ success: true, message: "Vous êtes connecté !" });
+  } else {
+    return res.status(401).json({ success: false, message: "Mot de passe incorrect" });
+  }
+});
+
+module.exports = router;
