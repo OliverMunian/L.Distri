1fdab166cd2f932808b123b44ec7d5e831b3d0bd
Olivier <omalahel@gmail.com>
2025-04-08 18:47:23 +0200

    update srv mongoose

--- Backend/model/connection.js
+++ Backend/model/connection.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(process.env.CONNECTION_STRING, { connectTimeoutMS: 2000 })
-  .then(() => console.log("Database connected"))
+  .then(() => console.log("Database connected - CONNECTION_STRING =", process.env.CONNECTION_STRING))
   .catch((error) => console.error('ligne 6: ',error));
