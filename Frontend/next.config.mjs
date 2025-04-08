cdb64f45ea28463b180b2d2f9f471f2f187a2ebd
Olivier <omalahel@gmail.com>
2025-04-08 11:51:36 +0200

    update next.config/mjs

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-      appDir: true, // active l'App Router
-  },
   images: {
     remotePatterns: [
       {
