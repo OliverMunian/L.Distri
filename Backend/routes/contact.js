bf3d2a752edaf68628db6ad19da16881f7f1433c
Olivier <omalahel@gmail.com>
2025-04-11 10:31:52 +0200

    modify route contact

--- Backend/routes/contact.js
+++ Backend/routes/contact.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: ".env.local" });
 const express = require("express");
 const nodemailer = require("nodemailer");
 const path = require("path");
