462f1f84296f67de25650c9e66ef9fef0ef50133
Olivier Malahel <62344268+OliverMunian@users.noreply.github.com>
2025-04-12 15:55:36 +0200

    ex: fix(backend): Use MONGODB_URI env var and fix syntax
    Changement des variable de connection MongoDB

--- Backend/model/connection.js
+++ Backend/model/connection.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
+    const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.CONNECTION_STRING, { connectTimeoutMS: 2000 })
-  .then(() => console.log("Database connected - CONNECTION_STRING =", process.env.CONNECTION_STRING))
-  .catch((error) => console.error('ligne 6: ',error));
+    mongoose
+      .connect(process.env.MONGODB_URI, { connectTimeoutMS: 2000 })
+      .then(() => console.log("Database connected - MONGODB_URI =", process.env.MONGODB_URI))
+      .catch(error => console.error("Mongoose Connection Error:", error));
+    
