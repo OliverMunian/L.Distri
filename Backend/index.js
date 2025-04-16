4b4197b72c4c74bd07d5d3f7bf003e90cd599c83
Olivier <omalahel@gmail.com>
2025-04-16 15:13:19 +0200

    modify index.js

--- Backend/index.js
+++ Backend/index.js
@@ -46,10 +46,10 @@ app.use(
 );
 
 console.log("Avant app.listen()");
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
-// });
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
 });
+// app.listen(PORT, () => {
+//   console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
+// });
 console.log("Après app.listen()");
